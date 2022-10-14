c709067f72eeb5d2afae1e8bbc408adcab800c0c
Kelvin Soh <kelvinsoh@pop-os.localdomain>
2022-10-14 12:01:46 +0800

    feat sampleN

--- packages/mathlify/src/random/sample.ts
+++ packages/mathlify/src/random/sample.ts
@@ -1,4 +1,5 @@
 import { getRandomInt } from './getRandomInt';
+import { shuffle } from './shuffle';
 
 /**
  * picks a random element in an array
@@ -8,4 +9,23 @@ export function sample<T>(arr: T[]): T {
 		throw new Error(`pick does not work with empty array.`);
 	}
 	return arr[getRandomInt(0, arr.length - 1)];
+}
+
+/**
+ * picks n random elements *without* replacement
+ */
+export function sampleN<T>(n: number, arr: T[]): T[] {
+	if (!Number.isInteger(n) || n <= 0) {
+		throw new Error(`n must be a positive integer.`);
+	}
+	if (arr.length < n) {
+		throw new Error(`n must be bigger than the size of the array.`);
+	}
+	let arrDuplicate = [...arr];
+	const samples: T[] = [];
+	for (let i = 0; i < n; i++) {
+		arrDuplicate = shuffle(arrDuplicate);
+		samples.push(arrDuplicate.pop()!);
+	}
+	return samples;
 }
