df1605d6a33477812c8fad237313ed257fc1e5e1
Kelvin Soh <kelvinsoh@pop-os.localdomain>
2023-08-01 22:24:50 +0800

    feat expression constructor accepts string

--- packages/mathlify/src/core/algebra/expression/__express.test.ts
+++ packages/mathlify/src/core/algebra/expression/__express.test.ts
@@ -4,26 +4,31 @@ import { Fraction, Term, Expression } from '../../index.js';
 const uExp1a = new Expression(2).plus(9);
 const uExp1b = new Expression(new Fraction(-2)).plus(9);
 const uExp1c = new Expression(2).plus(-9);
-const uExp1d = new Expression({term: -2, brackets: 'always'}).plus(
-	-9
-);
+const uExp1d = new Expression({ term: -2, brackets: 'always' }).plus(-9);
 const uExp2a = new Expression(4).minus(7);
 const uExp2c = new Expression(4).minus(-7);
-const uExp2d = new Expression({term: -4, brackets: 'auto'}).minus(
-	-7
+const uExp2d = new Expression({ term: -4, brackets: 'auto' }).minus(-7);
+const uExp4f = new Expression(
+	-33,
+	{ term: 33, addition: false },
+	new Term(-87)
 );
-const uExp4f = new Expression(-33, {term: 33, addition: false}, new Term(-87));
 const uExp4e = new Expression(new Term(-24), 16, -10);
-const uExp4h = new Expression({term: 27, addition: false}).minus(-19).minus(-24);
+const uExp4h = new Expression({ term: 27, addition: false })
+	.minus(-19)
+	.minus(-24);
 
-const uExpFraction1c = new Expression({term: new Term(new Fraction(4,5)), addition: false}).plus(new Fraction(3,10));
+const uExpFraction1c = new Expression({
+	term: new Term(new Fraction(4, 5)),
+	addition: false,
+}).plus(new Fraction(3, 10));
 
-const zero = new Expression(1, new Fraction(-2,3)).minus(new Fraction(1,3));
+const zero = new Expression(1, new Fraction(-2, 3)).minus(new Fraction(1, 3));
 const xPlus1 = new Expression(new Term('x'), 1);
-const xMinus1 = new Expression(new Term('x'), -1);
+const xMinus1 = new Expression('x', -1);
 
 test('Expression', () => {
-	expect(()=>new Expression()).to.throw();
+	expect(() => new Expression()).to.throw();
 	expect(`${uExp1a}`).to.equal('11');
 	expect(`${uExp1b}`).to.equal('7');
 	expect(`${uExp1c}`).to.equal('- 7');
@@ -39,4 +44,3 @@ test('Expression', () => {
 	expect(`${xPlus1}`).to.equal('x + 1');
 	expect(`${xMinus1}`).to.equal('x - 1');
 });
-
--- packages/mathlify/src/core/algebra/expression/expression.js
+++ packages/mathlify/src/core/algebra/expression/expression.js
@@ -11,49 +11,55 @@ export class Expression {
 	/**
 	 * @constructor
 	 * Creates an Expression instance
-   * TODO: brackets handling, allow string
-	 * @param {(number|Fraction|Term|{term: number|Fraction|Term, brackets?: 'off'|'auto'|'always', addition?: boolean})[]} terms -
+	 * TODO: brackets handling, allow string
+	 * @param {(number|Fraction|string|Term|{term: number|Fraction|Term, brackets?: 'off'|'auto'|'always', addition?: boolean})[]} terms -
 	 * the terms are added by default and
 	 * brackets is 'off' by default.
 	 */
 	constructor(...terms) {
-		if (terms.length === 0) throw new Error('Expression must have at least one term');
+		if (terms.length === 0)
+			throw new Error('Expression must have at least one term');
 		/** @type {Map<string,Fraction>} */
 		const termMap = new Map();
 		terms.forEach((term) => {
 			if (typeof term === 'number' || term instanceof Fraction) {
-        const currentConstant = termMap.get('') ?? new Fraction(0);
-        termMap.set('', currentConstant.plus(term));
-      } else if (term instanceof Term){
-        const currentConstant = termMap.get(term.variable) ?? new Fraction(0);
-        termMap.set(term.variable, currentConstant.plus(term.coeff));
-      } else {
-        if (typeof term.term === 'number' || term.term instanceof Fraction){
-          const currentConstant = termMap.get('') ?? new Fraction(0);
-          if (term.addition === false) {
-            termMap.set('', currentConstant.minus(term.term));
-          } else {
-            termMap.set('', currentConstant.plus(term.term));
-          }
-        } else { // term.term of Term type
-          const currentConstant = termMap.get(term.term.variable) ?? new Fraction(0);
-          if (term.addition === false) {
-            termMap.set('', currentConstant.minus(term.term.coeff));
-          } else {
-            termMap.set('', currentConstant.plus(term.term.coeff));
-          }
-        }
-      }
+				const currentConstant = termMap.get('') ?? new Fraction(0);
+				termMap.set('', currentConstant.plus(term));
+			} else if (typeof term === 'string') {
+				const currentConstant = termMap.get(term) ?? new Fraction(0);
+				termMap.set(term, currentConstant.plus(1));
+			} else if (term instanceof Term) {
+				const currentConstant = termMap.get(term.variable) ?? new Fraction(0);
+				termMap.set(term.variable, currentConstant.plus(term.coeff));
+			} else {
+				if (typeof term.term === 'number' || term.term instanceof Fraction) {
+					const currentConstant = termMap.get('') ?? new Fraction(0);
+					if (term.addition === false) {
+						termMap.set('', currentConstant.minus(term.term));
+					} else {
+						termMap.set('', currentConstant.plus(term.term));
+					}
+				} else {
+					// term.term of Term type
+					const currentConstant =
+						termMap.get(term.term.variable) ?? new Fraction(0);
+					if (term.addition === false) {
+						termMap.set('', currentConstant.minus(term.term.coeff));
+					} else {
+						termMap.set('', currentConstant.plus(term.term.coeff));
+					}
+				}
+			}
 		});
 		this.termMap = termMap;
-    /** @type {Term[]} */
-    const termsArray = [];
-    termMap.forEach((coeff, variable) => {
-      if (coeff.is.not.zero()){
-        termsArray.push(new Term(coeff, variable));
-      }
-    });
-    this.terms = termsArray;
+		/** @type {Term[]} */
+		const termsArray = [];
+		termMap.forEach((coeff, variable) => {
+			if (coeff.is.not.zero()) {
+				termsArray.push(new Term(coeff, variable));
+			}
+		});
+		this.terms = termsArray;
 		this.kind = 'expression';
 		this.type = this.terms.length <= 1 ? 'expression-term' : 'expression';
 	}
@@ -71,22 +77,22 @@ export class Expression {
 	 * @returns {Expression} - the new Expression
 	 */
 	minus(x) {
-		return new Expression(...this.terms, {term: x, addition: false});
+		return new Expression(...this.terms, { term: x, addition: false });
 	}
 
 	/** toString
 	 * @returns {string} - the LaTeX string representation of the Expression
 	 */
 	toString() {
-    if (this.terms.length === 0) {
-      return '0';
-    }
-    return this.terms.reduce((prev,term,i)=>{
-      if (i!==0 && term.coeff.is.positive()){
-        return `${prev} + ${term}`
-      }
-      const space = i===0 ? '' : ' ';
-      return `${prev}${space}${term}`
-    }, '')
-  }
+		if (this.terms.length === 0) {
+			return '0';
+		}
+		return this.terms.reduce((prev, term, i) => {
+			if (i !== 0 && term.coeff.is.positive()) {
+				return `${prev} + ${term}`;
+			}
+			const space = i === 0 ? '' : ' ';
+			return `${prev}${space}${term}`;
+		}, '');
+	}
 }
