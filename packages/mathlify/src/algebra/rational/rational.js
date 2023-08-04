89a583aa0e1d2157ea3c603d48270371f7c6dd66
Kelvin Soh <kelvinsoh@pop-os.localdomain>
2023-08-04 22:01:06 +0800

    term fractional display mode rewrite

--- packages/mathlify/src/algebra/rational/rational.js
+++ packages/mathlify/src/algebra/rational/rational.js
@@ -7,7 +7,7 @@
 // of the RationalTerm class and typesetting is lost. TODO: fix this
 /** @see Expression  */
 
-import { Expression, Fraction, Term } from "../../core/index.js";
+import { Expression, Fraction, Term } from '../../core/index.js';
 
 //TODO: use lcm for constant denominator addition
 
@@ -19,157 +19,156 @@ import { Expression, Fraction, Term } from "../../core/index.js";
  * @property {"rational"|"rational-expression"} type - mathlify rational class type
  */
 export class RationalTerm extends Term {
-  /**
-   * @constructor
-   * Creates a Rational Term instance
-   * @param {Expression|number|Fraction|string|Term|(number|Fraction|string|Term)[]} numerator - the numerator
-   * @param {Expression|number|Fraction|string|Term|(number|Fraction|string|Term)[]} [denominator=1] - the denominator
-   * @throws {Error} if denominator is zero
-   */
-  constructor(numerator, denominator = 1) {
-    if (!(numerator instanceof Expression)) {
-      numerator = Array.isArray(numerator)
-        ? new Expression(...numerator)
-        : new Expression(numerator);
-    }
-    if (!(denominator instanceof Expression)) {
-      denominator = Array.isArray(denominator)
-        ? new Expression(...denominator)
-        : new Expression(denominator);
-    }
-    if (`${denominator}` === "0") {
-      throw new Error("denominator cannot be zero");
-    }
-    // simplifies the expression if the denominator is constant
-    if (denominator.is.constant()) {
-      const coeffs = [denominator.cast.toFraction()];
-      numerator.terms.forEach((term) => {
-        coeffs.push(term.coeff);
-      });
-      const divisor = Fraction.gcd(...coeffs);
-      numerator = numerator.divide(divisor);
-      denominator = denominator.divide(divisor);
-    }
-    super(`(${numerator})`, [`(${denominator})`, -1]);
-    this.num = numerator;
-    this.den = denominator;
-    this.kind = "rational";
-    this.type = `${denominator}` === "1" ? "rational-expression" : "rational";
-  }
+	/**
+	 * @constructor
+	 * Creates a Rational Term instance
+	 * @param {Expression|number|Fraction|string|Term|(number|Fraction|string|Term)[]} numerator - the numerator
+	 * @param {Expression|number|Fraction|string|Term|(number|Fraction|string|Term)[]} [denominator=1] - the denominator
+	 * @throws {Error} if denominator is zero
+	 */
+	constructor(numerator, denominator = 1) {
+		if (!(numerator instanceof Expression)) {
+			numerator = Array.isArray(numerator)
+				? new Expression(...numerator)
+				: new Expression(numerator);
+		}
+		if (!(denominator instanceof Expression)) {
+			denominator = Array.isArray(denominator)
+				? new Expression(...denominator)
+				: new Expression(denominator);
+		}
+		if (`${denominator}` === '0') {
+			throw new Error('denominator cannot be zero');
+		}
+		// simplifies the expression by dividing by GCD
+		// (k1 num / k2 num)
+		const k1 = numerator.gcd();
+		const k2 = denominator.gcd();
+		const coeff = k1.divide(k2);
+		const numDivisor = k1.divide(coeff.num);
+		const denDivisor = k2.divide(coeff.den);
+		numerator = numerator.divide(numDivisor);
+		denominator = denominator.divide(denDivisor);
+		super(`(${numerator})`, [`(${denominator})`, -1]);
+		this.num = numerator;
+		this.den = denominator;
+		this.kind = 'rational';
+		this.type = `${denominator}` === '1' ? 'rational-expression' : 'rational';
+	}
 
-  /**
-   * multiplication
-   * @param {number|Fraction|string|Term|Expression|RationalTerm} x - the other term/expression to multiply with
-   * @returns {RationalTerm} the product of the two
-   */
-  times(x) {
-    // (this.num * x.num) / (this.den * x.den)
-    const xRational = x instanceof RationalTerm ? x : new RationalTerm(x);
-    return new RationalTerm(
-      this.num.times(xRational.num),
-      this.den.times(xRational.den)
-    );
-  }
+	/**
+	 * multiplication
+	 * @param {number|Fraction|string|Term|Expression|RationalTerm} x - the other term/expression to multiply with
+	 * @returns {RationalTerm} the product of the two
+	 */
+	times(x) {
+		// (this.num * x.num) / (this.den * x.den)
+		const xRational = x instanceof RationalTerm ? x : new RationalTerm(x);
+		return new RationalTerm(
+			this.num.times(xRational.num),
+			this.den.times(xRational.den)
+		);
+	}
 
-  /**
-   * division
-   * @param {number|Fraction|string|Term|Expression|RationalTerm} x - the other term/expression to divide with
-   * @returns {RationalTerm} the quotient of the two
-   */
-  divide(x) {
-    // (this.num * x.den) / (this.den * x.num)
-    const xRational = x instanceof RationalTerm ? x : new RationalTerm(x);
-    return new RationalTerm(
-      this.num.times(xRational.den),
-      this.den.times(xRational.num)
-    );
-  }
+	/**
+	 * division
+	 * @param {number|Fraction|string|Term|Expression|RationalTerm} x - the other term/expression to divide with
+	 * @returns {RationalTerm} the quotient of the two
+	 */
+	divide(x) {
+		// (this.num * x.den) / (this.den * x.num)
+		const xRational = x instanceof RationalTerm ? x : new RationalTerm(x);
+		return new RationalTerm(
+			this.num.times(xRational.den),
+			this.den.times(xRational.num)
+		);
+	}
 
-  /**
-   * addition
-   * @param {number|Fraction|string|Term|Expression|RationalTerm} x - term/expression to be added
-   * @returns {RationalTerm} - the sum of the two
-   */
-  plus(x) {
-    // (this.num * x.den + this.den * x.num) / (this.den * x.den)
-    const xRational = x instanceof RationalTerm ? x : new RationalTerm(x);
-    const num = this.num
-      .times(xRational.den)
-      .plus(this.den.times(xRational.num));
-    const den = this.den.times(xRational.den);
-    return new RationalTerm(num, den);
-  }
+	/**
+	 * addition
+	 * @param {number|Fraction|string|Term|Expression|RationalTerm} x - term/expression to be added
+	 * @returns {RationalTerm} - the sum of the two
+	 */
+	plus(x) {
+		// (this.num * x.den + this.den * x.num) / (this.den * x.den)
+		const xRational = x instanceof RationalTerm ? x : new RationalTerm(x);
+		const num = this.num
+			.times(xRational.den)
+			.plus(this.den.times(xRational.num));
+		const den = this.den.times(xRational.den);
+		return new RationalTerm(num, den);
+	}
 
-  /**
-   * negative
-   * @returns {RationalTerm} the negative of the expression
-   */
-  negative() {
-    return new RationalTerm(this.num.negative(), this.den);
-  }
+	/**
+	 * negative
+	 * @returns {RationalTerm} the negative of the expression
+	 */
+	negative() {
+		return new RationalTerm(this.num.negative(), this.den);
+	}
 
-  /**
-   * subtraction
-   * @param {number|Fraction|string|Term|Expression|RationalTerm} x - term/expression to be subtracted
-   * @returns {RationalTerm} - the difference this minus x
-   */
-  minus(x) {
-    if (typeof x === "number" || typeof x === "string") {
-      x = new Term(x);
-    }
-    return this.plus(x.negative());
-  }
+	/**
+	 * subtraction
+	 * @param {number|Fraction|string|Term|Expression|RationalTerm} x - term/expression to be subtracted
+	 * @returns {RationalTerm} - the difference this minus x
+	 */
+	minus(x) {
+		if (typeof x === 'number' || typeof x === 'string') {
+			x = new Term(x);
+		}
+		return this.plus(x.negative());
+	}
 
-  /**
-   * sub in a value for a variable
-   * @param {{[key: string]: number|Fraction}|number|Fraction} variableToValue - the values to sub in with the key being the variable signature.
-   * @returns {RationalTerm} - the new RationalTerm
-   */
-  subIn(variableToValue) {
-    return new RationalTerm(
-      this.num.subIn(variableToValue),
-      this.den.subIn(variableToValue)
-    );
-  }
+	/**
+	 * sub in a value for a variable
+	 * @param {{[key: string]: number|Fraction}|number|Fraction} variableToValue - the values to sub in with the key being the variable signature.
+	 * @returns {RationalTerm} - the new RationalTerm
+	 */
+	subIn(variableToValue) {
+		return new RationalTerm(
+			this.num.subIn(variableToValue),
+			this.den.subIn(variableToValue)
+		);
+	}
 
-  /** methods to cast this term to other types */
-  cast = {
-    /**
-     * cast to Expression type (denominator must be a constant)
-     * @returns {Expression} an Expression representation of this term
-     */
-    toExpression: () => {
-      if (this.den.is.constant()) {
-        return this.num.divide(this.den.cast.toFraction());
-      }
-      throw new Error(
-        `cannot cast ${this} to Term: non-constant denominator detected`
-      );
-    },
-    /**
-     * cast to Term type
-     * @returns {Term} the term representation of this term
-     */
-    toTerm: () => {
-      return this.cast.toExpression().cast.toTerm();
-    },
-    /**
-     * cast to Fraction type
-     * @returns {Fraction} the fraction representation of this term
-     */
-    toFraction: () => {
-      return this.cast.toExpression().cast.toFraction();
-    },
-  };
+	/** methods to cast this term to other types */
+	cast = {
+		/**
+		 * cast to Expression type (denominator must be a constant)
+		 * @returns {Expression} an Expression representation of this term
+		 */
+		toExpression: () => {
+			if (this.den.is.constant()) {
+				return this.num.divide(this.den.cast.toFraction());
+			}
+			throw new Error(
+				`cannot cast ${this} to Term: non-constant denominator detected`
+			);
+		},
+		/**
+		 * cast to Term type
+		 * @returns {Term} the term representation of this term
+		 */
+		toTerm: () => {
+			return this.cast.toExpression().cast.toTerm();
+		},
+		/**
+		 * cast to Fraction type
+		 * @returns {Fraction} the fraction representation of this term
+		 */
+		toFraction: () => {
+			return this.cast.toExpression().cast.toFraction();
+		},
+	};
 
-  /**
-   * toString
-   * @returns {string} - the LaTeX string representation of the Expression
-   */
-  toString() {
-    if (`${this.den}` === "1") {
-      return `${this.num}`;
-    }
-    return `\\frac{${this.num}}{${this.den}}`;
-  }
+	/**
+	 * toString
+	 * @returns {string} - the LaTeX string representation of the Expression
+	 */
+	toString() {
+		if (`${this.den}` === '1') {
+			return `${this.num}`;
+		}
+		return `\\frac{${this.num}}{${this.den}}`;
+	}
 }
--- packages/mathlify/src/core/algebra/expression/__expression.test.ts
+++ packages/mathlify/src/core/algebra/expression/__expression.test.ts
@@ -1,7 +1,7 @@
-import { test, expect } from "vitest";
-import { Fraction } from "../../fraction";
-import { Term } from "../term";
-import { Expression } from "./expression";
+import { test, expect } from 'vitest';
+import { Fraction } from '../../fraction';
+import { Term } from '../term';
+import { Expression } from './expression';
 
 const uExp1a = new Expression(2).plus(9);
 const uExp1b = new Expression(new Fraction(-2)).plus(9);
@@ -11,67 +11,70 @@ const uExp2a = new Expression(4).minus(7);
 const uExp2c = new Expression(4).minus(-7);
 const uExp2d = new Expression({ term: -4 }).minus(-7);
 const uExp4f = new Expression(
-  -33,
-  { term: 33, addition: false },
-  new Term(-87)
+	-33,
+	{ term: 33, addition: false },
+	new Term(-87)
 );
 const uExp4e = new Expression(new Term(-24), 16, -10);
 const uExp4h = new Expression({ term: 27, addition: false })
-  .minus(-19)
-  .minus(-24);
+	.minus(-19)
+	.minus(-24);
 
 const uExpFraction1c = new Expression({
-  term: new Term(new Fraction(4, 5)),
-  addition: false,
+	term: new Term(new Fraction(4, 5)),
+	addition: false,
 }).plus(new Fraction(3, 10));
 
 const zero = new Expression(1, new Fraction(-2, 3)).minus(new Fraction(1, 3));
-const xPlus1 = new Expression(new Term("x"), 1);
-const xMinus1 = new Expression("x", -1);
-const minusXPlus1 = new Expression({ term: "x", addition: false }, 1);
-const xPlus2 = new Expression({ term: "x", addition: true }, 2);
-const threeFifthX = new Term(new Fraction(3, 5), "x");
-threeFifthX.setFractionalDisplay();
+const xPlus1 = new Expression(new Term('x'), 1);
+const xMinus1 = new Expression('x', -1);
+const minusXPlus1 = new Expression({ term: 'x', addition: false }, 1);
+const xPlus2 = new Expression({ term: 'x', addition: true }, 2);
+const threeFifthX = new Term(new Fraction(3, 5), 'x');
+threeFifthX.setDisplayMode('always');
 const threeFifthXPlus1 = new Expression(threeFifthX, 1);
 
-test("Expression", () => {
-  expect(() => new Expression()).to.throw();
-  expect(`${uExp1a}`).to.equal("11");
-  expect(`${uExp1b}`).to.equal("7");
-  expect(`${uExp1c}`).to.equal("- 7");
-  expect(`${uExp1d}`).to.equal("- 11");
-  expect(`${uExp2a}`).to.equal("- 3");
-  expect(`${uExp2c}`).to.equal("11");
-  expect(`${uExp2d}`).to.equal("3");
-  expect(`${uExp4f}`).to.equal("- 153");
-  expect(`${uExp4e}`).to.equal("- 18");
-  expect(`${uExp4h}`).to.equal("16");
-  expect(`${uExpFraction1c}`).to.equal("- \\frac{1}{2}");
-  expect(`${zero}`).to.equal("0");
-  expect(`${xPlus1}`).to.equal("x + 1");
-  expect(`${xPlus2}`).to.equal("x + 2");
-  expect(`${xMinus1}`).to.equal("x - 1");
-  expect(`${minusXPlus1}`).to.equal("- x + 1");
-  expect(`${threeFifthXPlus1}`).to.equal("\\frac{3 x}{5} + 1");
-  expect(`${threeFifthXPlus1.subIn(5)}`).to.equal("4");
-  expect(`${new Expression([2, "x"], 1)}`).to.be.equal("2 x + 1");
+test('Expression', () => {
+	expect(() => new Expression()).to.throw();
+	expect(`${uExp1a}`).to.equal('11');
+	expect(`${uExp1b}`).to.equal('7');
+	expect(`${uExp1c}`).to.equal('- 7');
+	expect(`${uExp1d}`).to.equal('- 11');
+	expect(`${uExp2a}`).to.equal('- 3');
+	expect(`${uExp2c}`).to.equal('11');
+	expect(`${uExp2d}`).to.equal('3');
+	expect(`${uExp4f}`).to.equal('- 153');
+	expect(`${uExp4e}`).to.equal('- 18');
+	expect(`${uExp4h}`).to.equal('16');
+	expect(`${uExpFraction1c}`).to.equal('- \\frac{1}{2}');
+	expect(`${zero}`).to.equal('0');
+	expect(`${xPlus1}`).to.equal('x + 1');
+	expect(`${xPlus2}`).to.equal('x + 2');
+	expect(`${xMinus1}`).to.equal('x - 1');
+	expect(`${minusXPlus1}`).to.equal('- x + 1');
+	expect(`${threeFifthXPlus1}`).to.equal('\\frac{3 x}{5} + 1');
+	expect(`${threeFifthXPlus1.subIn(5)}`).to.equal('4');
+	expect(`${new Expression([2, 'x'], 1)}`).to.be.equal('2 x + 1');
 });
 
-test("Expression casting", () => {
-  const frac2 = new Fraction(1, 2);
-  const exp2 = new Expression(frac2);
-  const zeroExp = new Expression(1, -1);
-  // @ts-expect-error
-  expect(frac2.is.equalTo(exp2)).toBe(false);
-  expect(frac2.is.equalTo(exp2.cast.toFraction())).toBe(true);
-  expect(exp2.cast.toTerm().kind).to.equal("term");
-  expect(`${zeroExp}`).to.equal("0");
-  expect(`${zeroExp.cast.toTerm()}`).to.equal("0");
-  expect(`${zeroExp.cast.toFraction()}`).to.equal("0");
-  expect(() => {
-    new Expression(2, "x").cast.toFraction();
-  }).to.throw();
-  expect(() => {
-    new Expression(2, "x").cast.toTerm();
-  }).to.throw();
+test('Expression casting', () => {
+	const frac2 = new Fraction(1, 2);
+	const exp2 = new Expression(frac2);
+	const zeroExp = new Expression(1, -1);
+	// @ts-expect-error
+	expect(frac2.is.equalTo(exp2)).toBe(false);
+	expect(frac2.is.equalTo(exp2.cast.toFraction())).toBe(true);
+	expect(exp2.cast.toTerm().kind).to.equal('term');
+	expect(`${zeroExp}`).to.equal('0');
+	expect(() => {
+		zeroExp.gcd();
+	}).to.throw();
+	expect(`${zeroExp.cast.toTerm()}`).to.equal('0');
+	expect(`${zeroExp.cast.toFraction()}`).to.equal('0');
+	expect(() => {
+		new Expression(2, 'x').cast.toFraction();
+	}).to.throw();
+	expect(() => {
+		new Expression(2, 'x').cast.toTerm();
+	}).to.throw();
 });
