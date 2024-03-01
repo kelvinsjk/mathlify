c0c8752f672ca4a625f251b98ae9271bc443738b
Kelvin Soh <kelvinsjk@gmail.com>
2024-03-02 01:00:19 +0800

    feat: quadratic factorization

--- packages/mathlify/tests/algebra/polynomial/polynomial.test.ts
+++ packages/mathlify/tests/algebra/polynomial/polynomial.test.ts
@@ -20,4 +20,24 @@ test('factorize', () => {
 	expect(`${factorized}`).toBe('x^2\\left( x^2 - 1 \\right)');
 	expect(`${factorized.commonFactor}`).toBe('x^2');
 	expect(`${factorized.remainingFactor}`).toBe('x^2 - 1');
+
+	const x2Minus2xPlus1 = polynomial([1, -2, 1]);
+	expect(`${x2Minus2xPlus1}`).toBe('x^2 - 2x + 1');
+	let factorized2 = x2Minus2xPlus1.factorize.quadratic();
+	expect(`${factorized2}`).toBe('\\left( x - 1 \\right)^2');
+	const x2Minus2xMinus3 = polynomial([1, -2, -3]);
+	factorized2 = x2Minus2xMinus3.factorize.quadratic();
+	expect(`${factorized2}`).toBe('\\left( x + 1 \\right)\\left( x - 3 \\right)');
+	const twoX2MinusXMinus3 = polynomial([2, -1, -3]);
+	factorized2 = twoX2MinusXMinus3.factorize.quadratic();
+	expect(`${factorized2}`).toBe('\\left( x + 1 \\right)\\left( 2x - 3 \\right)');
+	const twoX2Minus8 = polynomial([2, 0, -8]);
+	factorized2 = twoX2Minus8.factorize.quadratic();
+	expect(`${factorized2}`).toBe('2\\left( x + 2 \\right)\\left( x - 2 \\right)');
+	const x2PlusX = polynomial([1, 1, 0]);
+	factorized2 = x2PlusX.factorize.quadratic();
+	expect(`${factorized2}`).toBe('x\\left( x + 1 \\right)');
+	const nineMinusY2 = polynomial([9, 0, -1], { ascending: true, variable: 'y' });
+	factorized2 = nineMinusY2.factorize.quadratic();
+	expect(`${factorized2}`).toBe('\\left( 3 + y \\right)\\left( 3 - y \\right)');
 });
